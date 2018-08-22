3913001692d92a040e06d83c9f71514f098662c0
Nikolay Rulev <nikolayr2001@gmail.com>
2018-08-23 00:35:34 +0300

    add postgres support and some sql commands

--- server/data/datamanager.py
+++ server/data/datamanager.py
@@ -1,8 +1,9 @@
 import asyncio
+import asyncpg
 import logging
 import socket
 import json
-
+from sql import *
 
 logger = logging.getLogger('data.DataServer')
 
@@ -19,9 +20,11 @@ class DataServer:
         self.master_socket.bind((host, port))
         self.master_socket.listen(socket.SOMAXCONN)
         self.loop = asyncio.get_event_loop()
+        self.pool = None
         logger.info(f'Start DataServer on {host}:{port}')
 
     async def serv(self):
+        self.loop = await asyncpg.create_pool('postgresql://cyan:cyan@localhost:5432/cyan')
         while True:
             sock, _ = await self.loop.sock_accept(self.master_socket)
             logger.info('new connection')
@@ -50,21 +53,56 @@ class DataServer:
 
     async def solve(self, request):
         if request[0] == 'USER':
-            return await self.solve_user(request[1:])
+            return await self._solve_user(request[1:])
 
         elif request[0] == 'GROUP':
-            return await self.solve_group(request[1:])
+            return await self._solve_group(request[1:])
 
         elif request[0] == 'FILE':
-            return await self.solve_file(request[1:])
+            return await self._solve_file(request[1:])
 
-    async def solve_user(self, request):
+    async def _solve_user(self, request):
+        user = int(request[1][1:])
+        if request[0] == 'POST':
+            data = json.loads(request[-1].decode())
+            if request[1] == 'u000000':  # CREATE NEW USER
+                # (email, password, display_name, registration_utc)
+                async with self.pool.acquire() as conn:
+                    result = await conn.fetchrow(CREATE_NEW_USER,
+                                                 data['email'],
+                                                 data['password'],
+                                                 data['display_name'],
+                                                 data['registration_utc'])
+                return json.dumps(dict(result))
+
+            else:  # UPDATE USER INFO
+
+                # get user meta from db
+                async with self.pool.acquire() as conn:
+                    meta = await conn.fetch(USER_META, data['USER'])
+
+                # (id, email, password, display_name, registration_utc, avatar, description)
+                async with self.pool.acquire() as conn:
+                    result = await conn.fetch(UPDATE_USER,
+                                              int(request[1][1:]),
+                                              data.setdefault('email', meta['email']),
+                                              data.setdefault('password', meta['password']),
+                                              data.setdefault('display_name', meta['display_name']),
+                                              data.setdefault('registration_utc', meta['registration_utc']),
+                                              data.setdefault('avatar', meta['avatar']),
+                                              data.setdefault('description', meta['description']))
+        else:
+            async with self.pool.acquire() as conn:
+                result = await conn.fetchrow(USER_META, user)
+            return json.dumps(dict(result))
+
+    async def _solve_group(self, request):
         pass
 
-    async def solve_group(self, request):
+    async def _solve_file(self, request):
         pass
 
-    async def solve_file(self, request):
+    async def _user_post(self):
         pass
 
     async def handle_connection(self, sock):
--- server/data/sql.py
+++ server/data/sql.py
@@ -1,3 +1,26 @@
-CREATE_NEW_USER = """
 
+# params (email, password, display_name, registration_utc)
+CREATE_NEW_USER = """
+INSERT INTO users
+(email, password, display_name, registration_utc)
+VALUES
+($1, $2, $3, $4)
+RETURNING 
+(id);
+"""
+
+# params (id, email, password, display_name, registration_utc, avatar, description)
+UPDATE_USER = """
+UPDATE users 
+SET
+email = $2, password = $3, display_name = $4, registration_utc = $5, avatar = $6, description = $7
+WHERE id = $1;
+"""
+
+# params (id)
+# return (email, password, display_name, registration_utc, avatar, description)
+USER_META = """
+SELECT *
+FROM users
+WHERE id = $1;
 """
